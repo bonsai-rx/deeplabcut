83d369f597fe7c669aecc086586946fb2eb0dea6
glopesdev <glopesdev@users.noreply.github.com>
2020-04-18 01:49:43 +0100

    Add operator to select individual body parts

---
+++ Bonsai.DeepLabCut/GetBodyPart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.DeepLabCut
+{
+    [Description("Selects the body part with the specified name from each input pose.")]
+    public class GetBodyPart : Transform<Pose, BodyPart>
+    {
+        [Description("The name of the body part.")]
+        public string Name { get; set; }
+
+        public override IObservable<BodyPart> Process(IObservable<Pose> source)
+        {
+            return source.Select(pose => pose[Name]);
+        }
+    }
+}
