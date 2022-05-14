0f716c4287824685b7249fbf98d1f6c77aa4d262
glopesdev <glopesdev@users.noreply.github.com>
2022-05-14 20:20:22 +0100

    Refactor pose visualizer and make labels optional

---
+++ Bonsai.DeepLabCut.Design/ColorPalette.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace Bonsai.DeepLabCut.Design
+{
+    static class ColorPalette
+    {
+        static readonly Color[] BrightPastelPalette = new[]
+{
+            ColorTranslator.FromHtml("#418CF0"),
+            ColorTranslator.FromHtml("#FCB441"),
+            ColorTranslator.FromHtml("#E0400A"),
+            ColorTranslator.FromHtml("#056492"),
+            ColorTranslator.FromHtml("#BFBFBF"),
+            ColorTranslator.FromHtml("#1A3B69"),
+            ColorTranslator.FromHtml("#FFE382"),
+            ColorTranslator.FromHtml("#129CDD"),
+            ColorTranslator.FromHtml("#CA6B4B"),
+            ColorTranslator.FromHtml("#005CDB"),
+            ColorTranslator.FromHtml("#F3D288"),
+            ColorTranslator.FromHtml("#506381"),
+            ColorTranslator.FromHtml("#F1B9A8"),
+            ColorTranslator.FromHtml("#E0830A"),
+            ColorTranslator.FromHtml("#7893BE")
+        };
+
+        public static Color GetColor(int colorIndex)
+        {
+            if (colorIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(colorIndex));
+            }
+
+            return BrightPastelPalette[colorIndex % BrightPastelPalette.Length];
+        }
+    }
+}
--- Bonsai.DeepLabCut.Design/PoseVisualizer.cs
+++ Bonsai.DeepLabCut.Design/PoseVisualizer.cs
@@ -10,6 +10,7 @@ using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
+using System.Windows.Forms;
 using Font = System.Drawing.Font;
 
 [assembly: TypeVisualizer(typeof(PoseVisualizer), Target = typeof(Pose))]
@@ -18,18 +19,24 @@ namespace Bonsai.DeepLabCut.Design
 {
     public class PoseVisualizer : IplImageVisualizer
     {
-        const float LabelFontScale = 0.05f;
-
         Pose pose;
-        Mat convertBuffer;
         IplImage labelImage;
         IplImageTexture labelTexture;
+        ToolStripButton drawLabelsButton;
         Font labelFont;
+
+        public bool DrawLabels { get; set; } = false;
 
         public override void Load(IServiceProvider provider)
         {
             base.Load(provider);
-            convertBuffer = new Mat(1, 1, Depth.U8, 3);
+            drawLabelsButton = new ToolStripButton("Draw Labels");
+            drawLabelsButton.CheckState = CheckState.Checked;
+            drawLabelsButton.Checked = DrawLabels;
+            drawLabelsButton.CheckOnClick = true;
+            drawLabelsButton.CheckedChanged += (sender, e) => DrawLabels = drawLabelsButton.Checked;
+            StatusStrip.Items.Add(drawLabelsButton);
+
             VisualizerCanvas.Load += (sender, e) =>
             {
                 labelTexture = new IplImageTexture();
@@ -57,50 +64,57 @@ namespace Bonsai.DeepLabCut.Design
 
         protected override void RenderFrame()
         {
+            var drawLabels = DrawLabels;
             GL.Color4(Color4.White);
             base.RenderFrame();
 
             if (pose != null)
             {
-                GL.PointSize(5 * VisualizerCanvas.Height / 640f);
-                if (labelImage == null || labelImage.Size != pose.Image.Size)
-                {
-                    labelImage = new IplImage(pose.Image.Size, IplDepth.U8, 4);
-                    var emSize = VisualizerCanvas.Font.SizeInPoints * (labelImage.Height * LabelFontScale) / VisualizerCanvas.Font.Height;
-                    labelFont = new Font(VisualizerCanvas.Font.FontFamily, emSize);
-                }
-
-                labelImage.SetZero();
+                GL.PointSize(5 * VisualizerCanvas.Height / 480f);
                 GL.Disable(EnableCap.Texture2D);
                 GL.Begin(PrimitiveType.Points);
-                using (var labelBitmap = new Bitmap(labelImage.Width, labelImage.Height, labelImage.WidthStep, System.Drawing.Imaging.PixelFormat.Format32bppArgb, labelImage.ImageData))
-                using (var graphics = Graphics.FromImage(labelBitmap))
-                using (var format = new StringFormat())
+                for (int i = 0; i < pose.Count; i++)
                 {
-                    graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                    format.Alignment = StringAlignment.Center;
-                    format.LineAlignment = StringAlignment.Center;
-                    for (int i = 0; i < pose.Count; i++)
-                    {
-                        var hue = 180f - (i * 180f / pose.Count);
-                        convertBuffer[0] = new Scalar(hue, 255, 255);
-                        CV.CvtColor(convertBuffer, convertBuffer, ColorConversion.Hsv2Bgr);
-                        var color = convertBuffer[0];
 
-                        var bodyPart = pose[i];
-                        var position = bodyPart.Position;
-                        GL.Color3(color.Val2 / 255, color.Val1 / 255, color.Val0 / 255);
-                        GL.Vertex2(NormalizePoint(position, pose.Image.Size));
-                        graphics.DrawString(bodyPart.Name, labelFont, Brushes.White, position.X, position.Y);
-                    }
+                    var bodyPart = pose[i];
+                    var position = bodyPart.Position;
+                    GL.Color3(ColorPalette.GetColor(i));
+                    GL.Vertex2(NormalizePoint(position, pose.Image.Size));
                 }
                 GL.End();
-                
-                GL.Color4(Color4.White);
-                GL.Enable(EnableCap.Texture2D);
-                labelTexture.Update(labelImage);
-                labelTexture.Draw();
+
+                if (drawLabels)
+                {
+                    if (labelImage == null || labelImage.Size != pose.Image.Size)
+                    {
+                        const float LabelFontScale = 0.05f;
+                        labelImage = new IplImage(pose.Image.Size, IplDepth.U8, 4);
+                        var emSize = VisualizerCanvas.Font.SizeInPoints * (labelImage.Height * LabelFontScale) / VisualizerCanvas.Font.Height;
+                        labelFont = new Font(VisualizerCanvas.Font.FontFamily, emSize);
+                    }
+
+                    labelImage.SetZero();
+                    using (var labelBitmap = new Bitmap(labelImage.Width, labelImage.Height, labelImage.WidthStep, System.Drawing.Imaging.PixelFormat.Format32bppArgb, labelImage.ImageData))
+                    using (var graphics = Graphics.FromImage(labelBitmap))
+                    using (var format = new StringFormat())
+                    {
+                        graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                        format.Alignment = StringAlignment.Center;
+                        format.LineAlignment = StringAlignment.Center;
+                        for (int i = 0; i < pose.Count; i++)
+                        {
+                            var bodyPart = pose[i];
+                            var position = bodyPart.Position;
+                            graphics.DrawString(bodyPart.Name, labelFont, Brushes.White, position.X, position.Y);
+                        }
+                    }
+
+                    GL.Color4(Color4.White);
+                    GL.Enable(EnableCap.Texture2D);
+                    labelTexture.Update(labelImage);
+                    labelTexture.Draw();
+                }
             }
         }
     }
