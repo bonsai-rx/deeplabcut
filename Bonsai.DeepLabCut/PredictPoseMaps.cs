ddda8be7afc1b73befe43199a880755b9c00ee8f
glopesdev <glopesdev@users.noreply.github.com>
2022-05-17 09:09:25 +0100

    Generalize pose map prediction for batch operation

--- Bonsai.DeepLabCut/PredictPoseMaps.cs
+++ Bonsai.DeepLabCut/PredictPoseMaps.cs
@@ -1,4 +1,4 @@
-using OpenCV.Net;
+﻿using OpenCV.Net;
 using System;
 using System.ComponentModel;
 using System.Linq;
@@ -21,7 +21,10 @@ namespace Bonsai.DeepLabCut
         [Description("The path to the configuration YAML file containing joint labels.")]
         public string PoseConfigFileName { get; set; }
 
-        public override IObservable<PoseEstimation> Process(IObservable<IplImage> source)
+        IObservable<TResult> Process<TSource, TResult>(
+            IObservable<TSource> source,
+            Func<TSource, IplImage[]> sourceSelector,
+            Func<PoseEstimation[], TResult> resultSelector)
         {
             return Observable.Defer(() =>
             {
@@ -29,13 +32,22 @@ namespace Bonsai.DeepLabCut
                 TFSession.Runner runner = null;
                 var graph = TensorHelper.ImportModel(ModelFileName, out TFSession session);
                 var config = ConfigHelper.LoadPoseConfig(PoseConfigFileName);
-                return source.Select(input =>
+                return source.Select(value =>
                 {
-                    var tensorSize = input.Size;
-                    if (tensor == null || tensor.Shape[1] != tensorSize.Height || tensor.Shape[2] != tensorSize.Width)
+                    var input = sourceSelector(value);
+                    if (input.Length == 0)
+                    {
+                        throw new InvalidOperationException("Frame batch must have at least one frame.");
+                    }
+
+                    var tensorSize = input[0].Size;
+                    if (tensor == null ||
+                        tensor.Shape[0] != input.Length ||
+                        tensor.Shape[1] != tensorSize.Height ||
+                        tensor.Shape[2] != tensorSize.Width)
                     {
                         runner = session.GetRunner();
-                        tensor = TensorHelper.CreatePlaceholder(graph, runner, tensorSize);
+                        tensor = TensorHelper.CreatePlaceholder(graph, runner, tensorSize, input.Length);
                         if (config.LocationRefinement) runner.Fetch("Sigmoid", "pose/locref_pred/block4/BiasAdd");
                         else runner.Fetch("Sigmoid", "pose/part_pred/block4/BiasAdd");
                     }
@@ -45,11 +57,27 @@ namespace Bonsai.DeepLabCut
                     var output = runner.Run();
 
                     // Fetch the results from output
-                    var scoreMap = TensorHelper.GetTensorMaps(output[0]);
-                    var locRef = TensorHelper.GetTensorMaps(output[1]);
-                    return new PoseEstimation(input, config, scoreMap, locRef);
+                    var result = new PoseEstimation[input.Length];
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        var scoreMap = TensorHelper.GetTensorMaps(output[0], i);
+                        var locRef = TensorHelper.GetTensorMaps(output[1], i);
+                        result[i] = new PoseEstimation(input[0], config, scoreMap, locRef);
+                    }
+
+                    return resultSelector(result);
                 });
             });
+        }
+
+        public override IObservable<PoseEstimation> Process(IObservable<IplImage> source)
+        {
+            return Process(source, frame => new[] { frame }, result => result[0]);
+        }
+
+        public IObservable<PoseEstimation[]> Process(IObservable<IplImage[]> source)
+        {
+            return Process(source, frame => frame, result => result);
         }
     }
 }
