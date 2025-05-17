c09b5f9cfd5d3c5b6428a5a9d3055f8b73b61bc8
David Maas <d.maas+git@neurogears.org>
2025-05-16 23:21:20 -0500

    Add CI/CD and brought repository into compliance with modern standards

---
+++ docs/build.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()] param (
+    [string[]]$docfxArgs
+)
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+Push-Location $PSScriptRoot
+try {
+    $libPaths = @()
+    $libPaths += Get-ChildItem "..\artifacts\bin\*\release_net4*" -Directory | Select-Object -Expand FullName
+    $libPaths += "..\artifacts\package\release"
+
+    ./export-images.ps1 $libPaths
+    dotnet docfx metadata
+    dotnet docfx build $docfxArgs
+} finally {
+    Pop-Location
+}
